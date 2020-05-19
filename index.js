b963647c3b792f0981593cce6ac1b1f76d322cde
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-05-19 09:58:39 +0545

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,8 @@ This package can do all the functions of normal **[ytdl-core](https://npmjs.com/
 - **[AndrozDev](https://discord.gg/Qreejcu)**
 `;
 
-var converter = new showdown.Converter();
+var converter = new showdown.Converter({ 
+    extensions: [showdownHighlight] 
+});
 var html = converter.makeHtml(markdown);
 document.getElementById("data").innerHTML = html;
