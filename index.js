4faf7b110c2384e7656d4b156e9357b786d7e488
Snowflake <46562212+Snowflake107@users.noreply.github.com>
2020-06-18 14:25:51 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,72 +1,47 @@
-var markdown = `# Installing
-<hr><b>
-\`
+var markdown = `# [discord-ytdl-core](https://discord-ytdl-core.netlify.app "Documentation")
+Simple ytdl wrapper for discord bots with custom ffmpeg args support.
+
+# Documentation
+**[Discord YTDL Core](https://discord-ytdl-core.netlify.app "Discord YTDL Core documentation site")**
+
+# Installing
+
+\`\`\`sh
 npm i discord-ytdl-core
-\`
-</b><hr>
+\`\`\`
 
 [https://www.npmjs.com/package/discord-ytdl-core](https://www.npmjs.com/package/discord-ytdl-core)
 
 > Please install an Opus engine & FFmpeg before using this package.
 
-## **Supported Opus Engines:**
-- **[@discordjs/opus](https://npmjs.com/package/@discordjs/opus)** [Requires WBT]
-- **[node-opus](https://npmjs.com/package/node-opus)** - [Requires WBT]
+## **Supported Opus Engines**
+- **[@discordjs/opus](https://npmjs.com/package/@discordjs/opus)**
+- **[node-opus](https://npmjs.com/package/node-opus)**
 - **[opusscript](https://npmjs.com/package/opusscript)**
- 
-# How To Install FFmpeg
-- Install FFmpeg from the website: 
-**[https://ffmpeg.org](https://ffmpeg.org)**
 
-- Install FFmpeg from npmjs:
-<b>\`npm i -g ffmpeg-static\`</b>
-
-- Tutorial by **[@iCrawl](https://github.com/icrawl)**: 
-**[https://www.youtube.com/watch?v=SW-iKrT_nJs](https://www.youtube.com/watch?v=SW-iKrT_nJs)**
-
-# Discord YTDL Core Options
+# Options
 This package provides 2 extra options excluding ytdl-core options.
 They are: \`seek\` & \`encoderArgs\`.
-
-- <u>seek</u>: This parameter takes the time in seconds. 
+- seek: This option takes the time in seconds. 
 If this option is provided, it will return the stream from that frame.
 Seek option is provided here because discord.js seek doesn't work for \`ogg/opus\` & \`webm/opus\` stream.
-This option is ignored when the supplied parameter type isn't a number. Time should be provided in seconds.
-**Example:**
-<hr>
-\`\`\`js
-let url = "https://www.youtube.com/watch?v=2lNT6AcKYl8";
-let stream = ytdl(url, { seek: 10 }); // seek to 10s
-VoiceConnection.play(stream, { type: "opus" });
+This option is ignored when the supplied parameter type isn't a number.
 
-\`\`\`
-<hr>
-
-- <u>encoderArgs</u>: This parameter takes the Array of FFmpeg arguments.
+- encoderArgs: This option takes the Array of FFmpeg arguments.
 Invalid args will throw error and crash the process.
 This option is ignored when the supplied parameter type isn't array. Invalid FFmpeg args might crash the process.
-**Example:**
-<hr>
-\`\`\`js
-let url = "https://www.youtube.com/watch?v=2lNT6AcKYl8";
-let stream = ytdl(url, { encoderArgs: ["-af", "bass=g=10"] }); // increase the bass
-VoiceConnection.play(stream, { type: "opus" });
 
-\`\`\`
-<hr>
+- opusEncoded: This option takes a Boolean value. If true, it returns \`opus encoded\` stream.
+  If \`fmt\` option isn't provided, it returns \`converted\` stream type of discord.js. Other values returns \`unknown\` stream if \`opusEncoded\` is false.
+
+- fmt: Refers to custom stream format. Don't use this option for default value. Even though this option changes the format, 
+  it returns \`opus\` stream if \`opusEncoded\` is set to \`true\`. 
+
 - Other options are the options for **[ytdl-core](https://npmjs.com/package/ytdl-core)**.
-**Example:**
-<hr>
-\`\`\`js
-let url = "https://www.youtube.com/watch?v=2lNT6AcKYl8";
-let stream = ytdl(url, { filter: "audioonly", quality: "highestaudio" }); // filter, quality
-VoiceConnection.play(stream, { type: "opus" });
-
-\`\`\`
-<hr>
 
 # Example
-<hr>
+## Playing Opus Encoded Stream
+
 \`\`\`js
 const ytdl = require("discord-ytdl-core");
 const Discord = require("discord.js");
@@ -78,17 +53,18 @@ client.on("ready", () => {
 
 client.on("message", msg => {
     if (msg.author.bot || !msg.guild) return;
-    if (msg.content === "??play") {
+    if (msg.content === "!play") {
         if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
         let stream = ytdl("https://youtube.com/watch?v=ERu6jh_1gR0", {
             filter: "audioonly",
-            encoderArgs: ['-af', 'equalizer=f=40:width_type=h:width=50:g=10'] // FFmpeg args array (optional)
+            opusEncoded: true,
+            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
         });
         
         msg.member.voice.channel.join()
         .then(connection => {
             connection.play(stream, {
-                type: "opus" // type: opus is compulsory because this package returns opus stream
+                type: "opus"
             })
             .on("finish", () => {
                 msg.guild.me.voice.channel.leave();
@@ -99,51 +75,114 @@ client.on("message", msg => {
 
 client.login("TOKEN");
 \`\`\`
-<hr>
 
-# Updating the filters of current stream
-It can be done by using \`seek\` method & \`Dispatcher.streamTime\` of discord.js. You have to re-play the track with new filters and providing the time in seek option. This may cause delay but it will work. We are working to solve this problem in the future.
+## Unknown Stream
 
-# Example Bots
-- **[P74Y](https://github.com/Snowflake107/P74Y)**
+\`\`\`js
+const ytdl = require("discord-ytdl-core");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("ready")
+});
+
+client.on("message", msg => {
+    if (msg.author.bot || !msg.guild) return;
+    if (msg.content === "!play") {
+        if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
+        let stream = ytdl("https://youtube.com/watch?v=ERu6jh_1gR0", {
+            filter: "audioonly",
+            opusEncoded: false,
+            fmt: "mp3",
+            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+        });
+        
+        msg.member.voice.channel.join()
+        .then(connection => {
+            connection.play(stream, {
+                type: "unknown"
+            })
+            .on("finish", () => {
+                msg.guild.me.voice.channel.leave();
+            })
+        });
+    }
+});
+
+client.login("TOKEN");
+\`\`\`
+
+## Converted Stream
+
+\`\`\`js
+const ytdl = require("discord-ytdl-core");
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("ready")
+});
+
+client.on("message", msg => {
+    if (msg.author.bot || !msg.guild) return;
+    if (msg.content === "!play") {
+        if (!msg.member.voice.channel) return msg.channel.send("You're not in a voice channel?");
+        let stream = ytdl("https://youtube.com/watch?v=ERu6jh_1gR0", {
+            filter: "audioonly",
+            opusEncoded: false,
+            encoderArgs: ['-af', 'bass=g=10,dynaudnorm=f=200']
+        });
+        
+        msg.member.voice.channel.join()
+        .then(connection => {
+            connection.play(stream, {
+                type: "converted"
+            })
+            .on("finish", () => {
+                msg.guild.me.voice.channel.leave();
+            })
+        });
+    }
+});
+
+client.login("TOKEN");
+\`\`\`
+
+# Downloading the video
+
+\`\`\`js
+const ytdl = require("../index.js");
+const { createWriteStream } = require ("fs");
+
+const url = "https://www.youtube.com/watch?v=zrwTYozyzYA";
+
+let stream = ytdl(url, {
+    encoderArgs: ["-af", "asetrate=44100*1.25,bass=g=20,dynaudnorm=f=150"],
+    fmt: "mp3",
+    opusEncoded: false
+});
+
+stream.pipe(createWriteStream(__dirname+"/test.mp3"));
+
+\`\`\`
 
 # Other functions
-This package can do all the functions of normal **[ytdl-core](https://npmjs.com/package/ytdl-core)**.
+Visit **[ytdl-core](https://npmjs.com/package/ytdl-core)** for other functions.
 
 # Related
 - **[ytdl-core-discord](https://npmjs.com/package/ytdl-core-discord)**
 - **[discord-player](https://npmjs.com/discord-player)**
 
 # Developers
-- **[@Snowflake107](https://github.com/Snowflake107)** - Initial (JS)
-- **[@Androz2091](https://github.com/Androz2091)** - Rewrite (TS)
+- **[@Snowflake107](https://github.com/Snowflake107)**
+- **[@Androz2091](https://github.com/Androz2091)**
 
 # Join our Official Discord Server
 - **[Snowflake Development ❄️](https://discord.gg/uqB8kxh)**
 - **[AndrozDev](https://discord.gg/Qreejcu)**
 `;
-/*
-showdown.extension('highlight', => {
-  return [{
-    type: "output",
-    filter: (text, converter, options) => {
-      var left = "<pre><code\\b[^>]*>",
-          right = "</code></pre>",
-          flags = "g";
-      var replacement = (wholeMatch, match, left, right) => {
-      	var lang = (left.match(/class=\"([^ \"]+)/) || [])[1];
-        left = left.slice(0, 18) + 'hljs ' + left.slice(18);
-        if (lang && hljs.getLanguage(lang)) {
-          return left + hljs.highlight(lang, match).value + right;
-        } else {
-          return left + hljs.highlightAuto(match).value + right;
-        }
-      };
-      return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
-    }
-  }];
-});
-*/
+
 var converter = new showdown.Converter(/*{ extensions: ["highlight"] }*/);
 var html = converter.makeHtml(markdown);
 document.getElementById("data").innerHTML = html;
