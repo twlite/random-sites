1f8e7184847c07ddf7ab9eea2cc50b43280e2b12
Snowflake <devsnowflake107@gmail.com>
2020-08-06 18:00:44 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.clear()
+console.clear();
+
+function preview() {
+	let img = document.getElementById("imgg");
+	if (!img.value) return alert("Please select an image!");
+	let file = img.files[0];
+	const Reader = new FileReader();
+	Reader.onload = (e) => {
+		document.getElementById("preview").src = e.target.result;
+	};
+	
+	Reader.readAsDataURL(file);
+}
